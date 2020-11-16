a369f921ac4e9705aa3071a13bd4b229370e3fbb
cassa10 <cassanojoseluis@gmail.com>
2020-11-15 21:23:36 -0300

    Add connection postgres db (for future)

--- sucursal_crud/sucursal_crud/settings.py
+++ sucursal_crud/sucursal_crud/settings.py
@@ -81,6 +81,16 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    """
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+    """
 }
 
 
